fda588c9509ad38a2f9e32156f529f57538ff2dc
Shahd Mohamed Farouk  Mohamed <shahdmohamedfarouk1112@gmail.com>
2026-07-24 20:44:53 +0300

    Add files via upload

---
+++ Reactive/main.py
@@ -0,0 +1,23 @@
+from rules import decide_patient
+from testCases import get_example_patients
+
+test_cases = get_example_patients()
+
+passed = 0
+
+for test in test_cases:
+    result = decide_patient(**test["input"])
+
+    print("-" * 30)
+    print(test["name"])
+    print("Expected:", test["expected"])
+    print("Actual  :", result)
+
+    if result == test["expected"]:
+        print("PASS")
+        passed += 1
+    else:
+        print("FAIL")
+
+print("-" * 30)
+print(f"Passed {passed}/{len(test_cases)} Test Cases")
